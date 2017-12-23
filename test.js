181e80d708c88be21ed740664c4e12fe5aa3380c
Andrés Sánchez <andres@Andress-MacBook-Pro.local>
2017-12-23 12:29:08 -0600

    V2

--- test.js
+++ test.js
@@ -1,12 +1,3 @@
-const jst = require('./index.js');
-var db = new jst.jstore(__dirname + '/testdb.jst');
-
-var searchResult = db.search('members', {
-   name: 'foo', 
-   access: {
-      query: jst.greaterThan,
-      value: 0
-   }
-})
-
-console.log(searchResult);
+const jstore = require('./index');
+const JClient = jstore.Client;
+var db = new JClient('./db.jst');
